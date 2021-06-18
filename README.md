31ea1c3929dc25fd431a976bc98d992f9f407fd1
Leandro Briglio <lnbriglio@gmail.com>
2021-06-18 17:41:24 -0300

    Removes unused code, adds comments

--- README.md
+++ README.md
@@ -1,3 +1,50 @@
+# Spring Cloud Streams - Join KTables using functional style
+
+### Prerequisites
+
+- `Java 16` This is not a requirement for the solution, this just happen to be what I had during the setup
+- `Docker` Required to run local instances of kafka provided in `support/docker-compose.yml`
+
+### Key elements
+
+`application.yml` the chosen binding style leverages configuration from this file, in here we define the input and
+output topics and which function should take care of handling that (among other things)
+
+Ref: https://docs.spring.io/spring-cloud-stream-binder-kafka/docs/3.0.10.RELEASE/reference/html/spring-cloud-stream-binder-kafka.html#_multiple_input_bindings
+
+### Overall flow and expectation of this code
+
+The provided function will listen to 2 topics (`products` and `vendors`) it's preferrable to have those topics created
+before lunching the spring application.
+
+Then we would want to send events to those topics with the intention of obtaining enriched products (products that
+include the vendor name as an output).
+
+Another requirement would be that if we send products before the vendors, those products wouldn't be enriched since no
+vendor data is available, but after we produce the vendor events, previously processed products should be re-evaluated
+for enrichment, and assuming the join condition matches we would then obtain enriched product events.
+
+### Sample data:
+
+Should send individual objects and use the key property as the EVENT KEY and serialize the value as a JSON string for
+the EVENT VALUE
+
+```javascript
+products = [
+    {'key': 1, 'value': {'id': 1234, 'price': 200, 'vendorId': 1}},
+    {'key': 555, 'value': {'id': 555, 'price': 600, 'vendorId': 1}},
+    {'key': 2, 'value': {'id': 111, 'price': 9, 'vendorId': 2}},
+    {'key': 222, 'value': {'id': 222, 'price': 1000, 'vendorId': 2}}
+]
+```
+
+```javascript
+vendors = [
+    {'key': 1, 'value': {'id': 1, 'name': 'MegaCorp'}},
+    {'key': 2, 'value': {'id': 2, 'name': 'DualProductions'}}
+]
+```
+
 https://www.ru-rocker.com/2020/08/11/how-to-join-one-to-many-and-many-to-one-relationship-with-kafka-streams/
 
 https://docs.spring.io/spring-cloud-stream-binder-kafka/docs/3.0.10.RELEASE/reference/html/spring-cloud-stream-binder-kafka.html#_multiple_input_bindings
--- src/main/java/com/calamar/ecom2/Ecom2Application.java
+++ src/main/java/com/calamar/ecom2/Ecom2Application.java
@@ -8,8 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.util.function.BiFunction;
-import java.util.function.Function;
-import java.util.stream.Stream;
 
 @SpringBootApplication
 public class Ecom2Application {
@@ -18,59 +16,23 @@ public class Ecom2Application {
         SpringApplication.run(Ecom2Application.class, args);
     }
 
-//	@Bean
-//	public Function<KStream<Object, WorkOrder>, KStream<Object, WorkOrder>> johnyConsumer(){
-//		return stream -> stream
-//				.peek((key, workOrder) ->
-//						System.out.println("Processing order: " + workOrder.id))
-//				.mapValues((key, workOrder) -> {
-//					WorkOrder modified = new WorkOrder();
-//					modified.id = workOrder.id;
-//					modified.status = "Processed";
-//
-//					return modified;
-//				});
-//	}
-//
-//	@Bean
-//	public Function<KStream<Object, WorkOrder>, KStream<Object, String>> notifyProcessed(){
-//		return stream -> stream
-//				.peek((key, workOrder) ->
-//						System.out.println("Notifying on order: " + workOrder + " Your order has been processed"))
-//				.mapValues(workOrder -> "Te procesamo la orden viteh " + workOrder.id)
-//				;
-//	}
-
-//    @Bean
-//    public BiFunction<KTable<String, Product>, KTable<String, Vendor>, KStream<String, String>> enrichProducts() {
-//        return (products, vendors) -> products
-//                .leftJoin(vendors,
-//                        (product, vendor) -> {
-//                            product.vendorName = vendor != null ? vendor.name : null;
-//                            return product;
-//                        }
-//                )
-//                .toStream()
-//                .peek((key, product) ->
-//                        System.out.println(String.format("<<<GOT THIS PRODUCT %s WITH PRICE %s VENDOR IS %s", product.id, product.price, product.vendorName)))
-//                .mapValues(product -> "Test")
-//                ;
-//    }
-
     @Bean
     public BiFunction<KTable<String, Product>, KTable<String, Vendor>, KStream<String, String>> enrichProducts() {
         return (products, vendors) -> products
-                .join(vendors, p -> p.vendorId,
-                        (p, v) -> {
-                            p.vendorName = v.name;
-                            return p;
+                // Join products with vendors, taking the KEY from the vendor event and choosing a matching property from the product
+                .join(vendors, product -> product.vendorId,
+                        // In case there is a match we will have a product and vendor
+                        (product, vendor) -> {
+                            // At this point we simple 'enrich' the product entity and return it to continue with our processing
+                            product.vendorName = vendor.name;
+                            return product;
                         }
                 )
                 .toStream()
                 .peek((key, product) ->
                         System.out.println(String.format("<<<GOT THIS PRODUCT %s WITH PRICE %s VENDOR IS %s", product.id, product.price, product.vendorName)))
-                .mapValues(product -> "Test")
-                ;
+                // This would be the output value for our stream
+                .mapValues(product -> "Test");
     }
 }
 
